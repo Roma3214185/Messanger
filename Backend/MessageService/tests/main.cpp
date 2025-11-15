79262e3802151fdd127a77ac8e00b5611af065f3
Роман Лобач <roma@MacBook-Air-Roman.local>
2025-11-15 14:14:45 +0200

    tests: refactor MessageService Cmake configuration for testing

---
+++ Backend/MessageService/tests/main.cpp
@@ -0,0 +1,8 @@
+#define CATCH_CONFIG_RUNNER
+#include <catch2/catch_all.hpp>
+
+int main(int argc, char* argv[]) {
+  Catch::Session session;
+  int result = session.run(argc, argv);
+  return result;
+}
---
+++ Backend/MessageService/tests/test_controller.cpp
@@ -0,0 +1,7 @@
+#include <catch2/catch_all.hpp>
+
+#include "controller.h"
+
+TEST_CASE("Firts test") {
+  REQUIRE(1 == 1);
+}
