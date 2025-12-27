e7b81268e76bf81ef9c520bcb4951099a9effe06
Роман Лобач <roma@MacBook-Air-Roman.local>
2025-12-27 22:11:19 +0200

    Add checkInvariants fucntion to Frontend entities

--- Frontend/include/dto/User.h
+++ Frontend/include/dto/User.h
@@ -9,6 +9,14 @@ struct User {
   QString name;
   long long id;
   QString avatarPath = "/Users/roma/QtProjects/Chat/default_avatar.jpeg";
+
+  bool checkInvariants() {
+    return id > 0 //todo: what if message saved as offline, them id will == 0, state pattern??
+           && !email.isEmpty()
+           && !name.isEmpty()
+           && !tag.isEmpty()
+           && !avatarPath.isEmpty();
+  }
 };
 
 #endif  // USER_H
