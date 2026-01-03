b188504390671f5615b9292ffb3a17468ec7ab9c
Роман Лобач <roma@MacBook-Air-Roman.local>
2026-01-03 20:53:22 +0200

    Implement QueryResult structure for Select and Delete Queries

---
+++ common/Persistence/include/query/QueryResult.h
@@ -0,0 +1,32 @@
+#ifndef QUERYRESULT_H
+#define QUERYRESULT_H
+
+#include "metaentity/EntityConcept.h"
+
+template <EntityJson T>
+struct SelectResult {
+    std::vector<T> result;
+    std::string error;
+};
+
+template <EntityJson T>
+struct DeleteResult {
+    bool success;
+    int affectedRows;
+    std::string error;
+};
+
+template <EntityJson T>
+using QueryResult = std::variant<SelectResult<T>, DeleteResult<T>>;
+
+template<typename V, typename Variant>
+struct is_in_variant;
+
+template<typename V, typename... Ts>
+struct is_in_variant<V, std::variant<Ts...>> : std::disjunction<std::is_same<V, Ts>...> {};
+
+template<typename V, typename Variant>
+concept VariantResultInQueryResult = is_in_variant<V, Variant>::value;
+//using QueryResult = Result<QueryValue, DbError>;
+
+#endif // QUERYRESULT_H
