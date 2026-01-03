e44ff6092ad7f2fff10831906689a34b76f6211b
Роман Лобач <roma@MacBook-Air-Roman.local>
2026-01-03 14:06:30 +0200

    Implement concept for Generic Repository and Queries

---
+++ common/Persistence/include/metaentity/EntityConcept.h
@@ -0,0 +1,22 @@
+#ifndef ENTITYCONCEPT_H
+#define ENTITYCONCEPT_H
+
+#include <nlohmann/json.hpp>
+#include "interfaces/entity.h"
+
+template<typename T>
+concept JsonRoundTrip = requires(const nlohmann::json& j, const T& t) {
+  { j.get<T>() } -> std::convertible_to<T>;
+  { nlohmann::json(t) } -> std::same_as<nlohmann::json>;
+};
+
+template<typename T>
+concept Entity =
+    std::derived_from<T, IEntity>;
+
+template<typename T>
+concept EntityJson =
+    Entity<T> &&
+    JsonRoundTrip<T>;
+
+#endif // ENTITYCONCEPT_H
