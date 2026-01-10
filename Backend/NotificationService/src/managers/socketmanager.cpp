d0d44e4689b5fdd04e3f5ee707e5b5db1dfa06e1
Роман Лобач <roma@MacBook-Air-Roman.local>
2026-01-10 15:04:52 +0200

    fix: fix sonar issues in Backend/NotificationService directory

--- Backend/NotificationService/src/managers/socketmanager.cpp
+++ Backend/NotificationService/src/managers/socketmanager.cpp
@@ -4,14 +4,14 @@
 #include "notificationservice/CrowSocket.h"
 
 void SocketsManager::saveConnections(UserId user_id, SocketPtr socket) {
-  std::lock_guard<std::mutex> lock(ws_mutex);
+ std::scoped_lock lock(ws_mutex_);
   user_sockets_[user_id] = socket;
 }
 
 void SocketsManager::deleteConnections(SocketPtr conn_to_delete) {  // todo: on close user send message (e.g "deinit")
-  std::lock_guard<std::mutex> lock(ws_mutex);
+ std::scoped_lock lock(ws_mutex_);
 
-  auto it = std::find_if(user_sockets_.begin(), user_sockets_.end(),
+  auto it = std::ranges::find_if(user_sockets_,
                          [&](const auto &p) { return p.second == conn_to_delete; });
   if (it != user_sockets_.end()) {
     user_sockets_.erase(it);
@@ -21,10 +21,10 @@ void SocketsManager::deleteConnections(SocketPtr conn_to_delete) {  // todo: on 
   }
 }
 
-bool SocketsManager::userOnline(UserId user_id) { return user_sockets_.find(user_id) != user_sockets_.end(); }
+bool SocketsManager::userOnline(UserId user_id) { return user_sockets_.contains(user_id); }
 
 SocketPtr SocketsManager::getUserSocket(UserId user_id) {
-  std::lock_guard<std::mutex> lock(ws_mutex);
+ std::scoped_lock lock(ws_mutex_);
   auto find = user_sockets_.find(user_id);
   if (find == user_sockets_.end()) {
     return nullptr;
