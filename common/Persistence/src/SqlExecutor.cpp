86035d807ed2c653a49b6b22a7785c6c362ceac2
Роман Лобач <roma@MacBook-Air-Roman.local>
2026-01-11 20:53:17 +0200

    fix: make class with saving results non-copyable to fix undefined results

--- common/Persistence/src/SqlExecutor.cpp
+++ common/Persistence/src/SqlExecutor.cpp
@@ -8,7 +8,7 @@ SqlExecutorResult SqlExecutor::execute(const QString &sql, const QList<QVariant>
   auto outQuery = database_.prepare(sql);
   if (!outQuery) {
     LOG_ERROR("Error prepare outQuery");
-    return SqlExecutorResult{.query = nullptr, .error = outQuery->error().toStdString()};
+    return SqlExecutorResult(outQuery->error().toStdString());
   }
 
   for (int i = 0; i < values.size(); ++i) outQuery->bind(values[i]);
@@ -17,9 +17,9 @@ SqlExecutorResult SqlExecutor::execute(const QString &sql, const QList<QVariant>
 
   if (!outQuery->exec()) {
     LOG_ERROR("[SqlExecutor] Exec failed: '{}'", sql.toStdString());
-    return SqlExecutorResult{.query = nullptr, .error = outQuery->error().toStdString()};
+    return SqlExecutorResult(outQuery->error().toStdString());
   }
 
   LOG_INFO("[SqlExecutor] Exec succeed: '{}'", sql.toStdString());
-  return SqlExecutorResult{.query = std::move(outQuery), .error = outQuery->error().toStdString()};
+  return SqlExecutorResult(std::move(outQuery));
 }
