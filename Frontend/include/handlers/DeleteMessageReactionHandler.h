9c415cd5b3c89be957372754d1a7b3ba84d5703c
Роман Лобач <roma@MacBook-Air-Roman.local>
2026-01-13 22:09:12 +0200

    feat: implement handlers to save/delete reactions in Frontend directory

---
+++ Frontend/include/handlers/DeleteMessageReactionHandler.h
@@ -0,0 +1,22 @@
+#ifndef DELETEMESSAGEREACTIONHANDLER_H
+#define DELETEMESSAGEREACTIONHANDLER_H
+
+#include <QJsonObject>
+#include "interfaces/ISocketResponceHandler.h"
+#include "managers/datamanager.h"
+
+class DeleteMessageReactionHandler : public ISocketResponceHandler {
+    EntityFactory* entity_factory_;
+    DataManager *data_manager_;
+
+  public:
+    DeleteMessageReactionHandler(EntityFactory *entity_factory, DataManager *data_manager)
+        : entity_factory_(entity_factory), data_manager_(data_manager) {}
+
+    void handle(const QJsonObject &json_object) override {
+      auto reaction = entity_factory_->getReaction(json_object); //entity_factory_->get<Reaction>(json_object);
+      data_manager_->deleteReaction(reaction);
+    }
+};
+
+#endif // DELETEMESSAGEREACTIONHANDLER_H
---
+++ Frontend/include/handlers/SaveMessageReactionHandler.h
@@ -0,0 +1,19 @@
+#ifndef SAVEMESSAGEREACTIONHANDLER_H
+#define SAVEMESSAGEREACTIONHANDLER_H
+
+
+class NewMessageResponceHandler : public ISocketResponceHandler {
+    EntityFactory* entity_factory_;
+    MessageUseCase *message_use_case_;
+
+  public:
+    NewMessageResponceHandler(EntityFactory *entity_factory, MessageUseCase *message_use_case)
+        : entity_factory_(entity_factory), message_use_case_(message_use_case) {}
+
+    void handle(const QJsonObject &json_object) override {
+      auto message = entity_factory_->getMessageFromJson(json_object);
+      message_use_case_->addMessageToChat(message);
+    }
+};
+
+#endif // SAVEMESSAGEREACTIONHANDLER_H
