66e8c7d6aa0137675b60c14bc4156b00f581da34
Роман Лобач <roma@MacBook-Air-Roman.local>
2026-01-15 19:56:06 +0200

    fix: Fix missing vtable error for MessageActionPanel class

--- Frontend/include/MessageActionPanel.h
+++ Frontend/include/MessageActionPanel.h
@@ -26,53 +26,8 @@ class FixedHeightDelegate : public QStyledItemDelegate {
 
 class MessageActionPanel : public QWidget {
     Q_OBJECT
-    std::vector<ReactionInfo> reactions_;
   public:
-    MessageActionPanel(const Message &msg, const std::vector<ReactionInfo>& reactions, QWidget *parent = nullptr)
-        : QWidget(parent), msg_(msg), reactions_(reactions)
-    {
-      auto *layout = new QVBoxLayout(this);
-      layout->setContentsMargins(4,4,4,4);
-      layout->setSpacing(4);
-
-      actionList_ = new QListView(this);
-      actionList_->setViewMode(QListView::ListMode);
-      actionList_->setHorizontalScrollBarPolicy(Qt::ScrollBarAlwaysOff);
-      actionList_->setVerticalScrollBarPolicy(Qt::ScrollBarAlwaysOff);
-      actionList_->setSizePolicy(QSizePolicy::Expanding, QSizePolicy::Preferred);
-      QFont font;
-      font.setPointSize(18);
-      actionList_->setFont(font);
-      int itemHeight = 24;
-      actionList_->setItemDelegate(new FixedHeightDelegate(itemHeight, actionList_)); // 24 px per item
-
-      actionModel_ = new QStringListModel({"Copy", "Edit", "Delete"}, this);
-      actionList_->setModel(actionModel_);
-
-      int itemCount = actionModel_->rowCount();
-      int spacing = actionList_->spacing();
-      int totalHeight = itemCount * itemHeight + (itemCount-1)*spacing + 2*actionList_->frameWidth();
-      actionList_->setFixedHeight(totalHeight);
-
-      layout->addWidget(actionList_);
-
-      emojiGrid_ = new QListView(this);
-      emojiGrid_->setViewMode(QListView::IconMode);
-      emojiGrid_->setSpacing(6);
-      emojiGrid_->setIconSize(QSize(18,18));
-      emojiGrid_->setResizeMode(QListView::Adjust);
-      emojiGrid_->setMovement(QListView::Static);
-      emojiGrid_->setFixedHeight(36);
-
-      emojiModel_ = new QStandardItemModel(this);
-      loadEmojiReactions();
-      emojiGrid_->setModel(emojiModel_);
-
-      layout->addWidget(emojiGrid_);
-
-      connect(actionList_, &QListView::clicked, this, &MessageActionPanel::onActionClicked);
-      connect(emojiGrid_, &QListView::clicked, this, &MessageActionPanel::onEmojiClicked);
-    }
+    MessageActionPanel(const Message &msg, const std::vector<ReactionInfo>& reactions, QWidget *parent = nullptr);
 
   Q_SIGNALS:
     void copyClicked(const Message &msg);
@@ -81,38 +36,16 @@ class MessageActionPanel : public QWidget {
     void reactionClicked(const Message &msg, long long emojiId);
 
   private:
-    void loadEmojiReactions() {
-      for (const auto &r : reactions_) {
-        QStandardItem *item = new QStandardItem;
-        QIcon icon(QString::fromStdString(r.image));
-        if(icon.isNull()) continue;
-        item->setIcon(icon);
-        item->setData(r.id, Qt::UserRole + 1);
-        emojiModel_->appendRow(item);
-      }
-    }
+    void loadEmojiReactions();
+    void onActionClicked(const QModelIndex &index);
+    void onEmojiClicked(const QModelIndex &index);
 
-    void onActionClicked(const QModelIndex &index) {
-      QString action = index.data().toString();
-      if (action == "Copy") Q_EMIT copyClicked(msg_);
-      else if (action == "Edit") Q_EMIT editClicked(msg_);
-      else if (action == "Delete") Q_EMIT deleteClicked(msg_);
-      else LOG_ERROR("Invalid action");
-      this->close();
-    }
-
-    void onEmojiClicked(const QModelIndex &index) {
-      long long emojiId = index.data(Qt::UserRole + 1).toInt();
-      Q_EMIT reactionClicked(msg_, emojiId);
-      this->close();
-    }
-
+    std::vector<ReactionInfo> reactions_;
     Message msg_;
     QListView *actionList_;
     QStringListModel *actionModel_;
     QListView *emojiGrid_;
     QStandardItemModel *emojiModel_;
 };
-
 
 #endif // MESSAGEACTIONPANEL_H
