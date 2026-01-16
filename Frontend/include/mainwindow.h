c349a2305a8608dfbec4b6bd127d5da94e3a2b56
Роман Лобач <roma@MacBook-Air-Roman.local>
2026-01-16 21:54:39 +0200

    refactor: refactor MainWindow class using ClickOutsideClosableListView class

--- Frontend/include/mainwindow.h
+++ Frontend/include/mainwindow.h
@@ -7,6 +7,7 @@
 #include <memory>
 
 #include "interfaces/IMainWindow.h"
+#include "OutsideClickFilter.h"
 
 QT_BEGIN_NAMESPACE
 namespace Ui {
@@ -20,6 +21,7 @@ class QListView;
 class ITheme;
 class MessageListView;
 class MessageDelegate;
+class ClickOutsideClosableListView;
 
 class MainWindow : public QMainWindow, public IMainWindow {
   Q_OBJECT
@@ -39,6 +41,12 @@ class MainWindow : public QMainWindow, public IMainWindow {
 
   void setTheme(std::unique_ptr<ITheme> theme);
 
+  bool eventFilter(QObject*, QEvent* event) override;
+
+ Q_SIGNALS:
+  void clickedOnPos(QPoint point);
+  void geometryChanged();
+
  private Q_SLOTS:
   void on_upSubmitButton_clicked();
   void on_inSubmitButton_clicked();
@@ -48,33 +56,27 @@ class MainWindow : public QMainWindow, public IMainWindow {
   void on_logoutButton_clicked();
   void on_pushButton_clicked(bool checked);
   void onPressEvent(QMouseEvent *event);
-
   void on_cancelEditButton_clicked();
-
   void on_okEditButton_clicked();
-
   void on_editTextEdit_textChanged();
-
   void setWriteMode();
-  // void setEditMode();
-
-  void on_serch_in_chat_button_clicked();
-
-  void on_cancel_search_messages_button_clicked();
-
   void on_search_messages_line_edit_textChanged(const QString &arg1);
-
   void on_serch_messages_list_view_clicked(const QModelIndex &index);
+  void closeEmojiMenu(bool close_manually = true);
+  void onEmojiButtonStateChanged(bool open);
 
- private:
-  void setMainWindow();
+private:
+  void setMainWindow() const;
+  void cancelSearchMessagesMode();
   void setSignInPage();
   void setSignUpPage();
   void clearUpInput();
   void setDelegators();
   void seupConnections();
   void setupUI();
-  void adjustSearchResultsHeight();
+  //void adjustSearchResultsHeight();
+  void openEmojiMenu();
+  void onEmojiClicked(const ReactionInfo& emoji);
 
   void copyMessage(const Message &message);
   void editMessage(const Message &message);
@@ -82,19 +84,26 @@ class MainWindow : public QMainWindow, public IMainWindow {
 
   void setSearchMessageMode();
   void setTitleChatMode();
+  QStandardItemModel* getEmojiModel();
+  void setupUserListView();
+  void setupSearchMessageListView();
 
   void onMessageContextMenu(const QPoint &pos);
   void onReactionClicked(const QPoint &pos);
   QModelIndex findIndexByMessageId(QAbstractItemModel *model, long long id);
 
   std::unique_ptr<ITheme> current_theme_;
-  std::unique_ptr<Ui::MainWindow> ui_;
+  Ui::MainWindow *ui;
   std::unique_ptr<Presenter> presenter_;
   std::unique_ptr<MessageListView> message_list_view_;
   MessageDelegate *message_delegate_;
   std::optional<Message> editable_message_;  // todo: make Page to set in currentPage, in which
                                              // Message will be
   std::unique_ptr<QStandardItemModel> searchResultsModel_;
+
+  ClickOutsideClosableListView* userListView_ = nullptr;
+  ClickOutsideClosableListView* searchMessageListView_ = nullptr;
+  ClickOutsideClosableListView* emoji_menu_ = nullptr;
 };
 
 #endif  // MAINWINDOW_H
