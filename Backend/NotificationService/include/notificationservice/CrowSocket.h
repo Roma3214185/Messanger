6e2b3b6fec21f94c83c22ebba446e85fb2a9cf89
Роман Лобач <roma@MacBook-Air-Roman.local>
2026-01-31 00:37:44 +0200

    Remove code from CrowSocket header in cpp

--- Backend/NotificationService/include/notificationservice/CrowSocket.h
+++ Backend/NotificationService/include/notificationservice/CrowSocket.h
@@ -3,31 +3,18 @@
 
 #include <crow.h>
 
-#include "Debug_profiling.h"
 #include "interfaces/ISocket.h"
 
 class CrowSocket final : public ISocket {
+ public:
+  explicit CrowSocket(crow::websocket::connection *conn);
+
+  bool isSameAs(crow::websocket::connection *other);
+  void send_text(const std::string &text) override;
+
+ private:
   crow::websocket::connection *conn_;
   std::mutex conn_mutex_;
-
- public:
-  explicit CrowSocket(crow::websocket::connection *conn) : conn_(conn) {}
-
-  bool isSameAs(crow::websocket::connection *other) { return other == conn_; }
-
-  void send_text(const std::string &text) override {
-    if (text.empty()) {
-      LOG_WARN("Text to send is empty");
-      return;
-    }
-
-    if (conn_) {
-      conn_->send_text(text);
-      LOG_INFO("{} is sended", text);
-    } else {
-      LOG_ERROR("{} is not sended, nullptr conn_", text);
-    }
-  }
 };
 
 #endif  // CROWSOCKET_H
---
+++ Backend/NotificationService/src/CrowSocket.cpp
@@ -0,0 +1,20 @@
+#include "notificationservice/CrowSocket.h"
+#include "Debug_profiling.h"
+
+CrowSocket::CrowSocket(crow::websocket::connection *conn) : conn_(conn) {}
+
+bool CrowSocket::isSameAs(crow::websocket::connection *other) { return other == conn_; }
+
+void CrowSocket::send_text(const std::string &text) {
+  if (text.empty()) {
+    LOG_WARN("Text to send is empty");
+    return;
+  }
+
+  if (conn_) {
+    conn_->send_text(text);
+    LOG_INFO("{} is sended", text);
+  } else {
+    LOG_ERROR("{} is not sended, nullptr conn_", text);
+  }
+}
