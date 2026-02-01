b9a6a2487847e8be7b33e5c56312399b31ba8d35
Роман Лобач <roma@MacBook-Air-Roman.local>
2026-02-01 22:40:35 +0200

    Impelement ISP principle for DataManager class

--- Frontend/include/managers/datamanager.h
+++ Frontend/include/managers/datamanager.h
@@ -22,23 +22,87 @@ using OptionalUser = std::optional<User>;
 using ListMessage = std::vector<Message>;
 using ReactionsById = std::unordered_map<long long, ReactionInfo>;
 
-class DataManager : public QObject {
+class MessageStatus;
+
+class IReactionDataManager {
+ public:
+  virtual ~IReactionDataManager() = default;
+
+  virtual void save(const ReactionInfo &reaction) = 0;
+  virtual void save(const Reaction &reaction) = 0;
+  virtual void deleteReaction(const Reaction &reaction) = 0;
+  virtual std::optional<ReactionInfo> getReactionInfo(long long id) = 0;
+  virtual std::vector<ReactionInfo> getEmojiesForMenu() = 0;
+};
+
+class IMessageStatusDataManager {
+ public:
+  virtual ~IMessageStatusDataManager() = default;
+  virtual void save(const MessageStatus &message_status) = 0;
+};
+
+class IMessageDataManager {
+ public:
+  virtual ~IMessageDataManager() = default;
+
+  virtual MessageModelPtr getMessageModel(ChatId) = 0;
+  virtual std::optional<Message> getMessageById(long long id) = 0;
+
+  virtual void save(const Message &message) = 0;
+  virtual void deleteMessage(const Message &msg) = 0;
+
+  virtual int getNumberOfMessageModels() const noexcept = 0;
+  virtual void clearAllMessageModels() = 0;
+};
+
+class IUserDataManager {
+ public:
+  virtual ~IUserDataManager() = default;
+
+  virtual OptionalUser getUser(UserId) = 0;
+  virtual void save(const User &user) = 0;
+  virtual void clearAllUsers() = 0;
+
+ protected:
+  virtual int getNumberOfExistingUsers() const noexcept = 0;
+};
+
+class IChatDataManager {
+ public:
+  virtual ~IChatDataManager() = default;
+
+  virtual ChatPtr getPrivateChatWithUser(UserId) = 0;
+  virtual ChatPtr getChat(ChatId) = 0;
+  virtual int getNumberOfExistingChats() const noexcept = 0;
+
+  virtual void save(const ChatPtr &chat, MessageModelPtr message_model = nullptr) = 0;
+
+  virtual void clearAllChats() = 0;
+};
+
+class DataManager : public QObject,
+                    public IChatDataManager,
+                    public IUserDataManager,
+                    public IMessageDataManager,
+                    public IReactionDataManager,
+                    public IMessageStatusDataManager {
   Q_OBJECT
  public:
-  [[nodiscard]] ChatPtr getPrivateChatWithUser(UserId);
-  [[nodiscard]] MessageModelPtr getMessageModel(ChatId);
-  [[nodiscard]] ChatPtr getChat(ChatId);
-  [[nodiscard]] int getNumberOfExistingChats() const noexcept;
-  void clearAllChats();  // todo: think about clear chats authoatically clear
-                         // message-models
-  void clearAllUsers();
-  void clearAllMessageModels();
-  void addChat(const ChatPtr &chat, MessageModelPtr message_model = nullptr);
+  [[nodiscard]] ChatPtr getPrivateChatWithUser(UserId) override;
+  [[nodiscard]] MessageModelPtr getMessageModel(ChatId) override;
+  [[nodiscard]] ChatPtr getChat(ChatId) override;
+  [[nodiscard]] int getNumberOfExistingChats() const noexcept override;
+  void clearAllChats() override;  // todo: think about clear chats authoatically clear
+                                  // message-models
+  void clearAllUsers() override;
+  void clearAllMessageModels() override;
+  void save(const ChatPtr &chat, MessageModelPtr message_model = nullptr) override;
 
-  void save(const User &user);
-  void save(const Message &message);
-  void save(const ReactionInfo &reaction_info);
-  void save(const Reaction &reaction);
+  void save(const User &user) override;
+  void save(const Message &message) override;
+  void save(const ReactionInfo &reaction_info) override;
+  void save(const Reaction &reaction) override;
+  void save(const MessageStatus &) override;
 
   template <typename T, template <typename...> class Container>
   void save(const Container<T> &entities) {
@@ -48,16 +112,16 @@ class DataManager : public QObject {
   }
 
   void clearAll();
-  [[nodiscard]] OptionalUser getUser(UserId);
-  [[nodiscard]] std::optional<Message> getMessageById(const long long id);
-  [[nodiscard]] int getNumberOfMessageModels() const noexcept {
+  [[nodiscard]] OptionalUser getUser(UserId) override;
+  [[nodiscard]] std::optional<Message> getMessageById(const long long id) override;
+  [[nodiscard]] int getNumberOfMessageModels() const noexcept override {
     return static_cast<int>(message_models_by_chat_id_.size());
   }
-  void deleteMessage(const Message &msg);
-  void readMessage(long long message_id, long long readed_by);
-  std::optional<ReactionInfo> getReactionInfo(long long reaction_id);
-  void deleteReaction(const Reaction &reaction);
-  std::vector<ReactionInfo> getEmojiesForMenu();
+
+  void deleteMessage(const Message &msg) override;
+  std::optional<ReactionInfo> getReactionInfo(long long reaction_id) override;
+  void deleteReaction(const Reaction &reaction) override;
+  std::vector<ReactionInfo> getEmojiesForMenu() override;
 
  Q_SIGNALS:
   void chatAdded(const ChatPtr &chat);
@@ -66,7 +130,7 @@ class DataManager : public QObject {
   void messageDeleted(const Message &message);
 
  protected:
-  [[nodiscard]] int getNumberOfExistingUsers() const noexcept;
+  [[nodiscard]] int getNumberOfExistingUsers() const noexcept override;
 
  private:
   auto getIterMessageById(long long message_id);
