a1d22dfc1cd070f4aba703c1b97404a6c242d5f1
Роман Лобач <roma@MacBook-Air-Roman.local>
2026-02-02 20:05:54 +0200

    Apply ISP principle to SocketRepository class

--- Backend/NotificationService/include/notificationservice/SocketRepository.h
+++ Backend/NotificationService/include/notificationservice/SocketRepository.h
@@ -7,16 +7,42 @@
 #include "interfaces/ISocket.h"
 
 using SocketPtr = std::shared_ptr<ISocket>;
+using UserId = long long;
+using UserSocketsMap = std::unordered_map<UserId, SocketPtr>;
+using AvtiveSockets = std::unordered_set<SocketPtr>;
 
-class SocketRepository {
+class IActiveSocketRepository {
+public:
+    virtual ~IActiveSocketRepository() = default;
+    virtual SocketPtr findSocket(crow::websocket::connection *conn) = 0;
+    virtual void addConnection(const SocketPtr &socket) = 0;
+    virtual void deleteConnection(const SocketPtr &socket) = 0;
+};
+
+class IUserSocketRepository {
+public:
+    virtual ~IUserSocketRepository() = default;
+    virtual void saveConnections(UserId, SocketPtr socket) = 0;
+    virtual SocketPtr getUserSocket(UserId) = 0;
+    virtual bool userOnline(UserId) = 0;
+};
+
+class SocketRepository
+    : public IActiveSocketRepository
+    , public IUserSocketRepository {
  public:
-  SocketPtr findSocket(crow::websocket::connection *conn);
-  void addConnection(const SocketPtr &socket);
-  void deleteConnection(const SocketPtr &socket);
+  SocketPtr findSocket(crow::websocket::connection *conn) override;
+  void addConnection(const SocketPtr &socket) override;
+  void deleteConnection(const SocketPtr &socket) override;
+  void saveConnections(UserId, SocketPtr socket) override;
+  SocketPtr getUserSocket(UserId) override;
+  bool userOnline(UserId) override;
 
  private:
-  std::unordered_set<SocketPtr> active_sockets_;
-  std::mutex ws_mutex_;
+  AvtiveSockets active_sockets_;
+  UserSocketsMap user_sockets_;
+  std::mutex ws_active_mutex_;
+  std::mutex ws_user_mutex_;
 };
 
 #endif  // SOCKETREPOSITORY_H
--- Backend/NotificationService/src/SocketRepository.cpp
+++ Backend/NotificationService/src/SocketRepository.cpp
@@ -1,9 +1,10 @@
 #include "notificationservice/SocketRepository.h"
 
 #include "notificationservice/CrowSocket.h"
+#include "Debug_profiling.h"
 
 SocketPtr SocketRepository::findSocket(crow::websocket::connection *conn) {
-  std::scoped_lock lock(ws_mutex_);
+  std::scoped_lock lock(ws_active_mutex_);
   for (auto &socket : active_sockets_) {
     if (auto crowSocket = std::dynamic_pointer_cast<CrowSocket>(socket)) {
       if (crowSocket->isSameAs(conn)) {
@@ -16,11 +17,41 @@ SocketPtr SocketRepository::findSocket(crow::websocket::connection *conn) {
 }
 
 void SocketRepository::addConnection(const SocketPtr &socket) {
-  std::scoped_lock lock(ws_mutex_);
+  std::scoped_lock lock(ws_active_mutex_);
   active_sockets_.insert(socket);
 }
 
-void SocketRepository::deleteConnection(const SocketPtr &socket) {
-  std::scoped_lock lock(ws_mutex_);
-  active_sockets_.erase(socket);
+void SocketRepository::saveConnections(UserId user_id, SocketPtr socket) {
+    std::scoped_lock lock(ws_user_mutex_);
+    user_sockets_[user_id] = std::move(socket);
 }
+
+void SocketRepository::deleteConnection(const SocketPtr &conn_to_delete) {  // todo: on close user send message (e.g "deinit")
+    std::scoped_lock lock(ws_user_mutex_);
+    std::scoped_lock lock2(ws_active_mutex_);
+
+    active_sockets_.erase(conn_to_delete);
+
+    auto it = std::ranges::find_if(user_sockets_, [&](const auto &p) { return p.second == conn_to_delete; });
+    if (it != user_sockets_.end()) {
+        user_sockets_.erase(it);
+        LOG_INFO("Deleted connection");
+    } else {
+        LOG_WARN("Connection to delete not found");
+    }
+}
+
+bool SocketRepository::userOnline(UserId user_id) {
+    std::scoped_lock lock(ws_user_mutex_);
+    return user_sockets_.contains(user_id);
+}
+
+SocketPtr SocketRepository::getUserSocket(UserId user_id) {
+    std::scoped_lock lock(ws_user_mutex_);
+    auto find = user_sockets_.find(user_id);
+    if (find == user_sockets_.end()) {
+        return nullptr;
+    }
+    return find->second;
+}
+
